3d546afd509796c5ee2726da12fd5b78e43c6d8e
Magooch <crvallecito@gmail.com>
2024-11-22 14:24:35 -0700

    Rearrange menu choices and remove remaining conditonals

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,14 @@ public class Main {
         Library library = new Library();
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Welcome to our Library Management System!");
         System.out.println();
-        System.out.println("To browse our library press 1");
-        System.out.println("To checkout a book press 2 to enter book title");
-        System.out.println("To checkout a book with ISBN, press 3 and enter ISBN");
-        System.out.println("To return a book press 4 and enter the title!");
-        System.out.println("To return a book with ISBN, press 5 and enter ISBN");
-        System.out.println("Press 6 to enter name of book and see details");
+        System.out.println("Welcome to our Library Management System!");
+        System.out.println("1. Browse our library");
+        System.out.println("2. Recieve details of a particular book");
+        System.out.println("3. Return your book by title");
+        System.out.println("4. Checkout a book with ISBN");
+        System.out.println("5. Checkout a book with title");
+        System.out.println("6. Return your book by ISBN");
         System.out.print("Choose your destiny: ");
         int choice = scanner.nextInt();
 
@@ -23,61 +23,7 @@ public class Main {
                 System.out.println();
                 library.displayBooks();
                 return;
-            } /* else if (choice == 2) {
-                // changes isCheckedOut to true if book is available and congratulates user
-                System.out.print("Enter title: ");
-                String title = scanner.nextLine();
-                if (library.contains(title) && isCheckedOut == false) {
-                    title.isCheckedOut == true;
-                    System.out.println("Congratulations, you have checked out " + title + "!");
-                    return;
-                } else {
-                    System.out.println("Sorry, this book is not in our library!");
-                    return;
-                }
-            } else if (choice == 3) {
-                // changes isCheckedOut to true if book is available and congratulates user
-                System.out.print("Enter ISBN number: ");
-                int ISBN = scanner.nextInt();
-                if (library.contains(ISBN) && isCheckedOut == false) {
-                    title.isCheckedOut == true;
-                    System.out.println("Congratulations, you have checked out " + this.isbn + "!");
-                    return;
-                } else {
-                    System.out.println("Sorry, this book is not in our library!");
-                    return;
-                }
-            } else if (choice == 4) {
-                // returns isCheckedOut to false
-                String title = scanner.nextLine();
-                if (title.isCheckedOut) {
-                    title.isCheckedOut = false;
-                    System.out.println("Thank you for returning " + title.name + "!");
-                    return;
-                } else {
-                    System.out.println("This book is not checked out! Would you like to check it out?");
-                    return;
-                }
-            } else if (choice == 5) {
-                // returns isCheckedOut to false
-                int ISBN = scanner.nextInt();
-                if (ISBN.isCheckedOut) {
-                    ISBN.isCheckedOut = false;
-                    System.out.println("Thank you for returning " + ISBN.name + "!");
-                    return;
-                } else {
-                    System.out.println("This book is not checked out! Would you like to check it out?");
-                    return;
-                }
-            } else if (choice == 6) {
-                // prints details of book and its availability
-                System.out.print("Enter title: ");
-                String title = scanner.nextLine();
-                printBook(title);
-            } else {
-                System.out.println("Invalid choice!");
-                return;
-            } */
+            } 
         }
     }
 }
