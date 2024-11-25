ac5b58a6306162fad302ac9d94e973eb98f140f7
Magooch <crvallecito@gmail.com>
2024-11-24 23:33:31 -0700

    Change if-else to switch-case, add try-catch as a way to accept both string and int input

--- src/Main.java
+++ src/Main.java
@@ -10,53 +10,71 @@ public class Main {
         while (true) {
         
             System.out.println();
-            System.out.println("1. Browse our library");
-            System.out.println("2. Recieve details of a particular book");
-            System.out.println("3. Checkout a book by title");
-            System.out.println("4. Checkout a book with ISBN");
-            System.out.println("5. Return your book by title");
-            System.out.println("6. Return your book with ISBN");
-            System.out.println("Type \"exit\" to exit program");
+            System.out.println("Enter \"help\" to bring up menu");
             System.out.print("Choose your destiny: ");
         
-            int choice = scanner.nextInt();
-            scanner.nextLine();
-        
-            if (choice == 1) {
-                System.out.println();
-                System.out.println("Please browse our library for a book that suits your interests!");
-                System.out.println();
-                library.displayBooks();
-            } else if (choice == 2) {
-                System.out.println();
-                System.out.print("Enter book title: ");
-                String title = scanner.nextLine();
-                library.bookDetails(title);
-            } else if (choice == 3) {
-                System.out.println();
-                System.out.print("Enter book to checkout: ");
-                String title = scanner.nextLine();
-                library.titleCheckout(title);
-            } else if (choice == 4) {
-                System.out.println();
-                System.out.print("Enter ISBN of book to checkout: ");
-                int num = scanner.nextInt();
-                library.isbnCheckout(num);
-            } else if (choice == 5) {
-                System.out.println();
-                System.out.print("Enter title of book you're returning: ");
-                String title = scanner.nextLine();
-                library.titleReturn(title);
-            } else if (choice == 6) {
-                System.out.println();
-                System.out.print("Enter ISBN of book you're returning: ");
-                int num = scanner.nextInt();
-                library.isbnReturn(num);
-            } else {
-                System.out.println("Invalid choice!");
-                System.out.println("Choose a number between 1 - 6");
-            }
+            String input = scanner.nextLine();
 
+            System.out.println();
+            switch (input.toLowerCase()) {
+            case "exit":
+                System.out.println("Exiting program...");
+                System.exit(0);
+                break;
+            case "help":
+                System.out.println("Enter \"exit\" to exit program");
+                System.out.println("1. Browse our library");
+                System.out.println("2. Recieve details of a particular book");
+                System.out.println("3. Checkout a book by title");
+                System.out.println("4. Checkout a book with ISBN");
+                System.out.println("5. Return your book by title");
+                System.out.println("6. Return your book with ISBN");
+                System.out.println("Type \"exit\" to exit program");
+                break;
+            default:
+                try {
+                    switch (Integer.parseInt(input)) {
+                    case 1: 
+                        System.out.println("Please browse our library for a book that suits your interests!");
+                        System.out.println();
+                        library.displayBooks();
+                        break;
+                    case 2:
+                        System.out.print("Enter book title: ");
+                        String title = scanner.nextLine();
+                        library.bookDetails(title);
+                        break;
+                    case 3:
+                        System.out.print("Enter book to checkout: ");
+                        String titleCheckout = scanner.nextLine();
+                        library.titleCheckout(titleCheckout);
+                        break;
+                    case 4:
+                        System.out.print("Enter ISBN of book to checkout: ");
+                        int numCheckout = scanner.nextInt();
+                        scanner.nextLine();
+                        library.isbnCheckout(numCheckout);
+                        break;
+                    case 5:
+                        System.out.print("Enter title of book you're returning: ");
+                        String titleReturn = scanner.nextLine();
+                        library.titleReturn(titleReturn);
+                        break;
+                    case 6:
+                        System.out.print("Enter ISBN of book you're returning: ");
+                        int numReturn = scanner.nextInt();
+                        scanner.nextLine();
+                        library.isbnReturn(numReturn);
+                        break;
+                    default:
+                        System.out.println("Invalid choice!");
+                        System.out.println("Choose a number between 1 - 6");
+                        break;
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid input: Enter number between 1 and 6, \"help\" to bring up menu, or \"exit\" to exit program!");
+                }
+            }
         }
     }
 }
